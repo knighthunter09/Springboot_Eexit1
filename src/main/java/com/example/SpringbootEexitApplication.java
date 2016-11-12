c647766e655f0cc1c6acebf2da86d5b07039ed00
knighthunter09 <emailforshekhar@gmail.com>
2016-11-12 14:42:48 +0800

    INit

--- src/main/java/com/example/SpringbootEexitApplication.java
+++ src/main/java/com/example/SpringbootEexitApplication.java
@@ -1,10 +1,22 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@SpringBootApplication
+@Controller
+@EnableAutoConfiguration
+//@SpringBootApplication
 public class SpringbootEexitApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World hi!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootEexitApplication.class, args);
